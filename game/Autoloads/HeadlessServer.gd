4503756c430e157c9dea91bcd981bc6377412581
Anand Mallik <ccanandmallik@gmail.com>
2023-11-25 02:45:17 -0500

    cleans up headless server file a bit

--- game/Autoloads/HeadlessServer.gd
+++ game/Autoloads/HeadlessServer.gd
@@ -1,24 +1,25 @@
 extends Node
 
 const PORT = 9999
-const USE_WS = true
+const USE_WEBSOCKETS = true
 
+func _is_server():
+	return "--server" in OS.get_cmdline_args()
 
 func _ready():
 	var _gc
 
-	if "--server" in OS.get_cmdline_args():
-		print("Server")
+	if _is_server():
+		print_debug("Server")
 
 		_gc = get_tree().connect("network_peer_connected", self, "_network_peer_connected")
 	else:
-		print("Client")
+		print_debug("Client")
 
 		_gc = get_tree().connect("connection_failed", self, "_client_connect_failed")
 		_gc = get_tree().connect("connected_to_server", self, "_client_connect_success")
 
-
-	if USE_WS:
+	if USE_WEBSOCKETS:
 		_setup_network_peer_as_ws()
 	else:
 		_setup_network_peer_as_udp()
@@ -27,14 +28,14 @@ func _ready():
 func _setup_network_peer_as_ws():
 	var peer
 
-	if "--server" in OS.get_cmdline_args():
+	if _is_server():
 		peer = WebSocketServer.new()
 		peer.listen(PORT, PoolStringArray(), true)
-		print("WS Server should be setup at port ", PORT)
+		print_debug("WS Server should be setup at port ", PORT)
 	else:
 		peer = WebSocketClient.new();
 		var url = "ws://%s:%s" % [GameConfig.nakama_host, PORT]
-		print("Attempting connection to ", url)
+		print_debug("Attempting connection to ", url)
 		peer.connect_to_url(url, PoolStringArray(), true);
 
 	get_tree().network_peer = peer
@@ -43,20 +44,20 @@ func _setup_network_peer_as_ws():
 func _setup_network_peer_as_udp():
 	var peer
 
-	if "--server" in OS.get_cmdline_args():
+	if _is_server():
 		peer = NetworkedMultiplayerENet.new()
 		peer.create_server(PORT, 8)
-		print("UDP Server should be setup at port ", PORT)
+		print_debug("UDP Server should be setup at port ", PORT)
 	else:
 		peer = NetworkedMultiplayerENet.new()
-		print("Attempting connection to ", GameConfig.nakama_host, " at port ", PORT)
+		print_debug("Attempting connection to ", GameConfig.nakama_host, " at port ", PORT)
 		peer.create_client(GameConfig.nakama_host, PORT)
 
 	get_tree().network_peer = peer
 
 
 func _exit_tree():
-	if "--server" in OS.get_cmdline_args():
+	if _is_server():
 		get_tree().disconnect("network_peer_connected", self, "_network_peer_connected")
 	else:
 		get_tree().disconnect("connection_failed", self, "_client_connect_failed")
@@ -64,13 +65,13 @@ func _exit_tree():
 
 
 func _client_connect_success():
-	print("client connect to server success")
+	print_debug("client connect to server success")
 
 
 func _client_connect_failed():
-	print("client connect to server failed")
+	print_debug("client connect to server failed")
 
 
 func _network_peer_connected(id):
-	print("network peer connected!")
-	print(id)
+	print_debug("network peer connected!")
+	print_debug(id)
