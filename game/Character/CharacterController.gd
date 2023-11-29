0d20e7ecd437baab9833721736e84d47869b5769
Anand Mallik <ccanandmallik@gmail.com>
2023-11-29 16:17:08 -0500

    server "kills" player

--- game/Character/CharacterController.gd
+++ game/Character/CharacterController.gd
@@ -39,14 +39,23 @@ remote func _fire_event():
 	var bullet: Area2D = bullet_scene.instance()
 	bullet.fire_dir = Vector2(0, 1).rotated(icon.rotation)
 	bullet.position = position
-	bullet.fired_from = PlayerManager.get_network_id()
+	bullet.fired_from = get_network_master()
+
 	get_parent().call_deferred("add_child", bullet)
 
 
 func take_damage():
 	starting_health -= DAMAGE_PER_BULLET
+	print_debug("player ", name, " has been hit")
 
-	if starting_health <= 0:
-		queue_free()
+	if PlayerManager.is_server() && starting_health <= 0:
+		print_debug("player ", name, " killed on server")
+		rpc("_player_killed")
+		_player_killed()
+
+
+# TODO: This should disappear the player temporarily, turn off collisions as well, all on a timer
+remote func _player_killed():
+	queue_free()
 
 
